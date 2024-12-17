72337396c0b871942a52aa5cd26dd89829e9e4f4
Fernando Mattioli <mattioli.fernando@gmail.com>
2024-12-17 10:59:23 -0300

    Adds material dependency.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {MatToolbar} from '@angular/material/toolbar';
+import {MatButton, MatIconButton} from '@angular/material/button';
+import {MatIcon} from '@angular/material/icon';
+import {MatSidenav, MatSidenavContainer, MatSidenavModule} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbar, MatIconButton, MatIcon, MatSidenavModule, MatButton, RouterLink,
+    RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,20 @@
 import { Routes } from '@angular/router';
 import {IndexComponent} from './components/index/index.component';
+import {ProjectsComponent} from './components/projects/projects.component';
 
 export const routes: Routes = [
   {
     path: "",
     component: IndexComponent
-  }
+  },
+
+  {
+    path: "home",
+    component: IndexComponent
+  },
+
+  {
+    path: "projects",
+    component: ProjectsComponent
+  },
 ];
